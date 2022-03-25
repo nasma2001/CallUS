350efd754bd13b467b5dfbba825691f1a8bb4004
Nesma <nasoma11125@gmail.com>
2022-03-25 17:00:44 +0200

    for testing

--- app/src/main/java/com/example/callus/MainActivity.java
+++ app/src/main/java/com/example/callus/MainActivity.java
@@ -3,19 +3,31 @@ package com.example.callus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences sharedPreferences;
+    String checkTheFirstTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(view -> {
-            Intent i = new Intent(this, SendVCode.class);
+        checkIfTheFirstTime();
+
+    }
+    private void checkIfTheFirstTime(){
+        sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
+        checkTheFirstTime = sharedPreferences.getString("installForTheFirstTime","");
+        if (checkTheFirstTime.equals("yes")){
+            Intent i = new Intent(this,SendVCode.class);
             startActivity(i);
-        });
+        }
+        else {
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putString("installForTheFirstTime","yes");
+            edit.apply();
+        }
     }
 }
--- app/src/main/java/com/example/callus/SendVCode.java
+++ app/src/main/java/com/example/callus/SendVCode.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,6 @@ public class SendVCode extends AppCompatActivity {
     Button btnContinue;
     EditText etPhoneNumber;
     ProgressBar progressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
