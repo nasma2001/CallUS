8d7c606661023a0ec81096baf09f3b76bac9cd11
Noor <noorqd2@gmail.com>
2022-03-29 22:15:13 +0300

    I Add Navigation Drawer.

---
+++ app/src/main/java/com/example/callus/NavegationDrawer/MY_LocationFragment.java
@@ -0,0 +1,65 @@
+package com.example.callus.NavegationDrawer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.callus.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MY_LocationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MY_LocationFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MY_LocationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MY_LocationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MY_LocationFragment newInstance(String param1, String param2) {
+        MY_LocationFragment fragment = new MY_LocationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_m_y__location, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/callus/NavegationDrawer/scan_QR_codeFragment.java
@@ -0,0 +1,65 @@
+package com.example.callus.NavegationDrawer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.callus.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link scan_QR_codeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class scan_QR_codeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public scan_QR_codeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment scan_QR_codeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static scan_QR_codeFragment newInstance(String param1, String param2) {
+        scan_QR_codeFragment fragment = new scan_QR_codeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan__q_r_code, container, false);
+    }
+}
